b7af078f8fda806614a06fe1d269bb900d5e72e4
jmsb8888 <80297741+jmsb8888@users.noreply.github.com>
2021-03-08 20:48:55 -0500

    Add files via upload

---
+++ Banner.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banner;
+
+/**
+ *
+ * @author uptc
+ */
+public class Banner {
+  private String message;
+
+    public Banner(String message) {
+        this.message = message;
+    }
+  
+    public char[][] generate(Size size, char symbo){
+      // to do ..
+      return null; 
+    }
+   
+}
