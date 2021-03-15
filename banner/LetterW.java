35897ca51c88b9d1588ce914d596b0c024aa3cd1
jmsb8888 <80297741+jmsb8888@users.noreply.github.com>
2021-03-14 22:01:54 -0500

    nuevas clases

--- banner/LetterW.java
+++ banner/LetterW.java
@@ -1,14 +1,14 @@
 package banner;
 
-public class LetterV extends MyCharacter {
+public class LetterW extends MyCharacter {
 
-	public LetterV(char value, Size size, char symbol) {
+	public LetterW(char value, Size size, char symbol) {
 		super(value, size,symbol);
 	}
 	
 	public char[][] generate() {
-		new DiagonalStroke(area,symbol).generate(area.length/2,0);
-		new TransversalStroke(area,symbol).generate(area.length-2, area.length/2+1, area.length/2);
+		new DiagonalStroke(area,symbol).generate(4,0);
+		new TransversalStroke(area,symbol).generate(7,5,4);
 		new VerticalStroke(area,symbol).generate(0).generate(area[0].length-1);
 		new EmptyPoints(area, ' ').generate4(5,0, area.length-1).generate4(5,area[0].length-1, area.length-1);
 		return area;
